6a6ba6f590d0c5f590bd82bf1f62c167fe1b64f8
antish24 <aantenehh@gmail.com>
2024-02-05 08:59:05 -0800

    last 0ne

--- src/components/Cards/Player/Player.js
+++ src/components/Cards/Player/Player.js
@@ -3,16 +3,21 @@ import styles from './palyer.module.css'
 import PlayerCard from '../PlayerCard/PlayerCard'
 
 const Player = ({data,dropCard,Order,CantOrder,FinishTurn}) => {
+  const types=[
+    {id:1,type:'heart'},
+    {id:3,type:'flowers'},
+    {id:2,type:'dimond'},
+    {id:4,type:'spare'},
+  ]
   return (
     <div className={styles.cont}>
         <button className={styles.drawbtn} onClick={()=>FinishTurn(data.id)} disabled={data.draw||!(data.turn)}>{data.draw?"Draw":"Fin"}</button>
       <div className={styles.choosetype} style={{display:CantOrder?"none":"flex"}}>
-      <select onChange={(e)=>Order(e.target.value)} disabled={CantOrder}>
-        <option value='heart'>Heart</option>
-        <option value='dimond'>Dimond</option>
-        <option value='spare'>Spare</option>
-        <option value='flowers'>Flowers</option>
-      </select>
+        <div className={styles.list}>
+        {
+          types.map(l=><img src={`${l.type}/${l.type}.png`} key={l.id} className={styles.cardtypepic} onClick={()=>Order(l.type)}/>)
+        }
+        </div>
       </div>
       <div className={styles.playercard}>
         {data.cards.map((l,index)=><PlayerCard key={l.id} playerId={data.id} turn={data.turn} Order={Order} dropCard={dropCard} left={index * 45} {...l}/>)}
